02f5a62d4f1e726aba7b30b1ee28892e3c2c5c93
Andreas Neubauer <andreas.neubauer90@gmx.de>
2021-09-08 14:21:33 +0200

    Change fake scores calculation to torch_ones

--- sgan/losses.py
+++ sgan/losses.py
@@ -45,7 +45,7 @@ def gan_d_loss(scores_real, scores_fake):
     - loss: Tensor of shape (,) giving GAN discriminator loss
     """
     y_real = torch.ones_like(scores_real) * random.uniform(0.7, 1.2)
-    y_fake = torch.zeros_like(scores_fake) * random.uniform(0, 0.3)
+    y_fake = torch.ones_like(scores_fake) * random.uniform(0, 0.3)
     loss_real = bce_loss(scores_real, y_real)
     loss_fake = bce_loss(scores_fake, y_fake)
     return loss_real + loss_fake
@@ -97,9 +97,7 @@ def displacement_error(pred_traj, pred_traj_gt, consider_ped=None, mode='sum'):
         return loss
 
 
-def final_displacement_error(
-    pred_pos, pred_pos_gt, consider_ped=None, mode='sum'
-):
+def final_displacement_error(pred_pos, pred_pos_gt, consider_ped=None, mode='sum'):
     """
     Input:
     - pred_pos: Tensor of shape (batch, 2). Predicted last pos.
